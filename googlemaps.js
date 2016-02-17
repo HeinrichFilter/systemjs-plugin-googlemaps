b2f9c0d1484c3d30d5a54828782ca3719cb4cff4
alexander.taran <alexander.taran@everys.com>
2016-02-17 17:12:19 +0300

    Revert "attach script to head if there are no script elements present in DOM"
    This reverts commit 5b17694569dcebcb495257d74577a4c6ca0dac50.

--- googlemaps.js
+++ googlemaps.js
@@ -1,21 +1,19 @@
-function injectScript(src) {
-    return new Promise(function (resolve, reject) {
-        window["__google_maps_callback__"] = function () {
-            resolve('');
-        }
+function injectScript(src){
+  return new Promise(function(resolve, reject) {
+    window["__google_maps_callback__"] = function() {
+      resolve('');
+    }
 
-        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = src;
-        var t = document.getElementsByTagName('script')[0];
-        if (t) { t.parentNode.insertBefore(s, t); }
-        else { var h = document.getElementsByTagName("head")[0]; h.appendChild(s); }
-    });
+    var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = src;
+    var t = document.getElementsByTagName('script')[0]; t.parentNode.insertBefore(s,t);
+  });
 }
 
-exports.fetch = function (load) {
-    var scriptUrl = load.address
-    scriptUrl += (scriptUrl.indexOf('?') < 0) ? '?' : '&';
-    scriptUrl += "callback=__google_maps_callback__"
-    return injectScript(scriptUrl).then(function () {
-        return 'module.exports = google.maps';
-    });
+exports.fetch = function(load) {
+  var scriptUrl = load.address
+  scriptUrl += (scriptUrl.indexOf('?') < 0)? '?' : '&';
+  scriptUrl += "callback=__google_maps_callback__"
+  return injectScript(scriptUrl).then(function() {
+    return 'module.exports = google.maps';
+  });
 };
